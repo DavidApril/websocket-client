92400aabb5f7f4f759e24d592ee15dc133545b0c
David Agámez <davidagamez04@gmail.com>
2024-11-14 04:30:48 -0500

    feat: rendering messages of each client

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import './style.css'
-import {connectToServer} from "./socket-client.ts";
+import './style.css';
+import { connectToServer } from './socket-client.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
+  
    <h1>Websocket - Client</h1>
    <span id="server-status">Offline</span>   
    <ui id="clients-ul"></ui>
+   
+   <form id="message-form">
+   <input type="text" id="message-input" placeholder="Message" />
+   </form>
+
+   <h3>Messages</h3>
+   <ul id="messages-ul"></ul>
+   
   </div>
-`
+`;
 
 connectToServer();
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,28 +1,57 @@
 import {Manager, Socket} from "socket.io-client";
 
 export const connectToServer = () => {
-  const manager = new Manager('http://localhost:3001/socket.io/socket.io.js');
-  const socket = manager.socket('/')
-  addListeners(socket)
-}
+  const manager = new Manager("http://localhost:3001/socket.io/socket.io.js");
+  const socket = manager.socket("/");
+  addListeners(socket);
+};
 
 const addListeners = (socket: Socket) => {
-  const serverStatusLabel = document.querySelector('#server-status') as HTMLSpanElement
-  const clientsUl = document.querySelector('#clients-ul') as HTMLUListElement
+  const clientsUl = document.querySelector("#clients-ul") as HTMLUListElement;
+  const messageForm = document.querySelector(
+    "#message-form"
+  ) as HTMLFormElement;
+  const messageInput = document.querySelector(
+    "#message-input"
+  ) as HTMLInputElement;
+  const messagesUl = document.querySelector("#messages-ul") as HTMLUListElement;
+  const serverStatusLabel = document.querySelector(
+    "#server-status"
+  ) as HTMLSpanElement;
 
-  socket.on('connect', () => {
-    serverStatusLabel.innerText = 'Connected'
-  })
+  socket.on("connect", () => {
+    serverStatusLabel.innerText = "Connected";
+  });
 
-  socket.on('disconnect', () => {
-    serverStatusLabel.innerText = 'Disconnected'
-  })
+  socket.on("disconnect", () => {
+    serverStatusLabel.innerText = "Disconnected";
+  });
 
-  socket.on('clients-updated', (clients: string[]) => {
-    let clientsHtml = ''
-    clients.forEach(client => {
-      clientsHtml += `<li>${client}</li>`
-    })
-    clientsUl.innerHTML = clientsHtml
-  })
-}
+  socket.on("clients-updated", (clients: string[]) => {
+    let clientsHtml = "";
+    clients.forEach((client) => {
+      clientsHtml += `<li>${client}</li>`;
+    });
+    clientsUl.innerHTML = clientsHtml;
+  });
+
+  socket.on(
+    "message-from-server",
+    (message: { fullName: string; message: string }) => {
+      const li = document.createElement("li");
+      li.innerText = `${message.fullName}: ${message.message}`;
+      messagesUl.appendChild(li);
+    }
+  );
+
+  messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (messageInput.value.trim().length === 0) return;
+    socket.emit("message-from-client", {
+      id: socket.id,
+      message: messageInput.value,
+    });
+
+    messageInput.value = "";
+  });
+};
